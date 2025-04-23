107a4b8906dfb900c3e2e9c8b5b21a160e7bbcd6
amalsiva7 <amalsiva7210@gmail.com>
2025-04-24 04:26:33 +0530

    added message_history view in dm_chat app

--- backend/dm_chat/views.py
+++ backend/dm_chat/views.py
@@ -3,8 +3,12 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
 from django.db.models import Q
-from .models import Match
+from users.models import Match
+from .models import *
 from .serializers import MatchedUserSerializer
+from rest_framework.pagination import PageNumberPagination
+
+
 
 
 class MatchedUsersListView(APIView):
@@ -24,3 +28,27 @@ class MatchedUsersListView(APIView):
         serializer = MatchedUserSerializer(matched_users, many=True, context={'current_user': user})
         return Response(serializer.data, status=status.HTTP_200_OK)
 
+
+class ChatHistoryView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request, chat_room_name):
+        # Retrieve the chat room
+        chat_room = ChatRoom.objects.get(name=chat_room_name)
+        messages = Message.objects.filter(room=chat_room).order_by('-timestamp')
+
+        # Pagination
+        paginator = PageNumberPagination()
+        paginator.page_size = 10  # Adjust page size to the number of messages you want to load initially
+        result_page = paginator.paginate_queryset(messages, request)
+
+        # Serialize messages and send response
+        message_data = [
+            {
+                "sender": message.sender.username,
+                "content": message.content,
+                "timestamp": message.timestamp,
+            }
+            for message in result_page
+        ]
+        return paginator.get_paginated_response(message_data)
